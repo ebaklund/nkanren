b34bdc4edb2e9c48fb40fe3350f81afccc9ea632
Erik Baklund <eb@fjordmaritime.no>
2024-03-30 19:45:36 +0100

    Added Suduko board renderer. Tests maintained on 1_80.

--- Soduko/Goals.cs
+++ Soduko/Goals.cs
@@ -9,7 +9,45 @@ namespace Soduko;
 
 internal static class AreaGoals
 {
-    /*
+    // PRIVATE
+/*
+    private static bool IsAbsent(this object[] a, Subst s, int val)
+    {
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (s.Walk(a[i]) is int v && v == val)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // PUBLIC
+    
+    public static Goal Exclusivo2(Key k, object[] row, object[] col, object[] lot) // p 154
+    {
+        return (Subst s) => 
+        {
+            var n = row.Length;
+            var w = s.Walk(k);
+
+            for (var i = 0; i < n; ++i)
+            {
+                if (w is Key kFresh)
+                {
+                    if (row.IsAbsent(s, i) || col.IsAbsent(s, i) || lot.IsAbsent(s, i))
+                    {
+                        yield return i;
+                    }
+
+                }
+            }
+        };
+    }
+
+ 
     // PRIVATE
 
     IEnumerator<int[]> Permute(int i, int[] vals, bool[] fresh)
--- Soduko/Program.cs
+++ Soduko/Program.cs
@@ -1,10 +1,114 @@
-﻿namespace Soduko
+﻿
+using static nk.Runners;
+using static nk.Goals;
+using static nk.Freshes;
+using static nk.ListConstructor;
+using static SodukoBoard;
+
+using System;
+using System.Text;
+using nk;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+RunAll(1, (q, x) => {
+    var board = Board
+    (
+        Row( 1 )
+    );
+
+    return Eqo(q, board);
+}).Render();
+
+RunAll(4, (q, x) => {
+    var board = Board
+    (
+        Row( x[0], x[1] ),
+        Row( x[2], x[3] )
+    );
+
+    return Eqo(q, board);
+}).Render();
+
+return 0;
+
+// HELPERS
+
+static class SodukoBoard
 {
-    internal class Program
+    // PRIVATE
+
+    private static string AsString(this object o)
     {
-        static void Main(string[] args)
+        return o switch
         {
-            Console.WriteLine("Hello, World!");
+            int i => $"{i}",
+              Key => " ",
+                _ => " ",
+        };
+    }
+
+    private static string[] AsStrings(this object[] objs)
+    {
+        string[] strings = new string[objs.Length];
+
+        for(int i = 0; i < objs.Length; ++i)
+        {
+            strings[i] = objs[i].AsString();
+        }
+
+        return strings;
+    }
+
+    private static void AppendRow(this StringBuilder sb, object[] row)
+    {
+        var dim = row.Length;
+        var strRow = row.AsStrings();
+
+        sb.Append("┃");
+
+        for (int i = 0; i < (dim - 1); ++i)
+        {
+            sb.Append(strRow[dim - 1] + "│");
+        }
+        
+        sb.Append(strRow[dim - 1] + "┃\n");
+    }
+
+    // PUBLIC
+
+    public static object[] Row(params object[] row)
+    {
+        return row;
+    }
+
+    public static object[] Board(params object[][] board)
+    {
+        return board;
+    }
+
+    public static void Render(this IEnumerator<object> e)
+    {
+        while (e.MoveNext())
+        {
+            var board = (object[][])e.Current;
+            var dim = board.Length;
+
+            var sb = new StringBuilder();
+            sb.Append("┏" + string.Concat(Enumerable.Repeat("━┯", dim - 1)) + "━┓\n");
+
+            for ( var i = 0; i < dim; i++ )
+            {
+                sb.AppendRow(board[i]);
+
+                if (i < dim - 1)
+                {
+                    sb.Append("┠" + string.Concat(Enumerable.Repeat("─┼", dim - 1)) + "─┨\n");
+                }
+            }
+    
+            sb.Append("┗" + string.Concat(Enumerable.Repeat("━┷", dim - 1)) + "━┛\n");
+            Console.Write(sb.ToString());
         }
     }
 }
