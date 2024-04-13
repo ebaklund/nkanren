19439b2e3952ae2a79a99731d39558f13481064b
Erik Baklund <eb@fjordmaritime.no>
2024-04-13 18:39:32 +0200

    Removed lots of dead Sudoku code.

--- Soduko/BoardModule/BoardRenderer.cs
+++ Soduko/BoardModule/BoardRenderer.cs
@@ -21,39 +21,12 @@ public static partial class BoardModule
         };
     }
 
-    private static string[] AsStrings(this object[] objs)
-    {
-        string[] strings = new string[objs.Length];
-
-        for (int i = 0; i < objs.Length; ++i)
-        {
-            strings[i] = AsString(objs[i]);
-        }
-
-        return strings;
-    }
-
     private static IEnumerator<string> AsStrings(this IEnumerator<object> values)
     {
         while (values.MoveNext())
         {
             yield return values.Current.AsString();
         }
-    }
-
-    private static void AppendRow(this StringBuilder sb, object[] row)
-    {
-        var dim = row.Length;
-        var strRow = AsStrings(row);
-
-        sb.Append("┃");
-
-        for (int i = 0; i < dim - 1; ++i)
-        {
-            sb.Append(strRow[i] + "│");
-        }
-
-        sb.Append(strRow[dim - 1] + "┃\n");
     }
 
     private static void AppendRow(this StringBuilder sb, IEnumerator<object> row)
@@ -72,33 +45,6 @@ public static partial class BoardModule
     }
 
     // PUBLIC
-
-    public static void ResetRenderCount()
-    {
-        _renderCount = 0;
-    }
-
-    public static string AsString(this object[][] board)
-    {
-        var dim = board.Dim();
-
-        var sb = new StringBuilder();
-        sb.Append("┏" + string.Concat(Enumerable.Repeat("━┯", dim - 1)) + "━┓\n");
-
-        for (var i = 0; i < dim; i++)
-        {
-            sb.AppendRow(board.Row(i));
-
-            if (i < dim - 1)
-            {
-                sb.Append("┠" + string.Concat(Enumerable.Repeat("─┼", dim - 1)) + "─┨\n");
-            }
-        }
-
-        sb.Append("┗" + string.Concat(Enumerable.Repeat("━┷", dim - 1)) + $"━┛ {++_renderCount}\n");
-
-        return sb.ToString();
-    }
 
     public static string AsString(this Board board, bool resetRenderCount = false)
     {
@@ -126,75 +72,6 @@ public static partial class BoardModule
         sb.Append("┗" + string.Concat(Enumerable.Repeat("━┷", rcount)) + $"━┛ {++_renderCount}\n");
 
         return sb.ToString();
-    }
-
-    public static IEnumerator<object[][]> AsBoards(this IEnumerator<object> stream)
-    {
-        while (stream.MoveNext())
-        {
-            if (stream.Current is object[][] matrix)
-            {
-                yield return matrix;
-                continue;
-            }
-
-            if (stream.Current is not object[] candidate)
-            {
-                throw new ApplicationException($">>>    This cannot be a Sudoku board! Unexpected type: \"{stream.Current.GetType().Name}\".    <<<");
-            }
-
-            int dim = (int)Math.Sqrt(candidate.Length);
-
-            if (dim*dim != candidate.Length)
-            {
-                throw new ApplicationException($">>>    This cannot be a Sudoku board! Not squareable array length: {candidate.Length}.    <<<");
-            }
-
-            var board = new object[dim][];
-            var i = 0;
-
-            for (var r = 0; r < dim; r++)
-            {
-                var row = new object[dim];
-
-                for (var c = 0; c < dim; c++)
-                {
-                    row[c] = candidate[i++];
-                }
-
-                board[r] = row;
-            }
-
-            yield return board;
-        }
-    }
-
-    public static IEnumerator<string> AsStrings(this IEnumerator<object[][]> boards)
-    {
-        _renderCount = 0;
-
-        while (boards.MoveNext())
-        {
-            yield return AsString(boards.Current);
-        }
-    }
-
-    public static IEnumerator<string> AsStrings(this IEnumerator<Board> boards)
-    {
-        _renderCount = 0;
-
-        while (boards.MoveNext())
-        {
-            yield return boards.Current.AsString();
-        }
-    }
-    
-    public static void ToConsole(this IEnumerator<string> strings)
-    {
-        while (strings.MoveNext())
-        {
-            Console.WriteLine(strings.Current, "\n");
-        }
     }
 }
 
--- nk/RunnerModule/Take.cs
+++ nk/RunnerModule/Take.cs
@@ -3,31 +3,6 @@ namespace nk;
 
 public static partial class RunnerModule
 {
-    public static bool TryTakeFirst<T>(this IEnumerator<T> s, out T? res) where T : class
-    {
-        res = null;
-
-        while(s.MoveNext())
-        {
-            res = s.Current;
-            return true;
-        }
-
-        return false;
-    }
-
-    public static bool TryTake<T>(this IEnumerator<T> s , int count, out List<T> res)
-    {
-        res = new List<T>();
-
-        while(s.MoveNext() && (res.Count < count))
-        {
-            res.Add (s.Current);
-        }
-
-        return res.Count == count;
-    }
-
     public static List<T> TakeMax<T>(this IEnumerator<T> stream , int n)
     {
         var count = 0;
